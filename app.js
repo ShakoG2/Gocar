5259b81fdefe9305607184bfab248c01a761f672
shakof <66114961+shako-art@users.noreply.github.com>
2021-02-06 14:02:20 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,106 @@
+const option=document.querySelectorAll(".options");
+const inputPriceType=document.getElementById("price-type");
+const selectOptions=document.getElementById("select-options")
+const selection1=document.querySelector('.select-options');
+const optionDiv=document.querySelector('.options');
+const typeSearch=document.querySelector('.type-search')
+
+window.addEventListener('click',function (e){
+    const select1=document.querySelector('#price-type');
+    const selection=document.querySelector('.select-options');
+    const triangleOne=document.querySelector('#arrow-selection');
+    const triangleTwo=document.querySelector('#arrow-selection-up');
+    if(!select1.contains(e.target)){
+        inputPriceType.style.borderColor='white';
+        selection.style.display='none';
+        triangleOne.classList.remove('arrow-selection-class');
+        triangleTwo.classList.remove('arrow-selection-up-class');
+    }
+})
+
+const inpContainer=document.querySelector(".input-container");
+const arrowSelection=document.querySelector("#arrow-selection");
+const arrowSelectionUp=document.querySelector("#arrow-selection-up");
+
+
+inpContainer.addEventListener('click',function (){
+    selection1.style.display='block';
+    // inputPriceType.setAttribute('class','price-type-class');
+    arrowSelection.setAttribute('class','arrow-selection-class');
+    arrowSelectionUp.setAttribute('class','arrow-selection-up-class');
+})
+
+
+    for( let i=0;i<option.length;i++){
+        option[i].addEventListener('click',function (){
+            inputPriceType.value=this.innerText
+        })
+    }
+
+
+const carsInput=document.querySelector('.cars-types-input');
+const selectOptionsDiv=document.querySelector('.cars-types-select-options');
+const carsTrOne=document.querySelector('#cars-arrow-selection');
+const carTrTwo=document.querySelector('#cars-arrow-selection-up');
+carsInput.addEventListener('click',function (){
+    selectOptionsDiv.style.display='block'
+    // carsInput.setAttribute('class','price-type-class');
+    typeSearch.classList.remove('type-search-reset-icon');
+    typeSearch.classList.add('type-search-icon');
+    carsTrOne.setAttribute('class','cars-t-arrow-selection');
+    carTrTwo.setAttribute('class','cars-t-arrow-selection-up');
+})
+window.addEventListener('click',(e)=>{
+    const select=document.querySelector('#cars-types-input');
+    const searchField=document.querySelector('#car-type-search')
+    if(!select.contains(e.target)&!searchField.contains(e.target)){
+        selectOptionsDiv.style.display='none';
+        // carsInput.style.borderColor='white';
+        carsTrOne.classList.remove('cars-t-arrow-selection');
+        carTrTwo.classList.remove('cars-t-arrow-selection-up');
+    }else{
+        // carsInput.style.borderColor='white';
+    }
+})
+const searchFieldValue=document.querySelector('.cars-types-input');
+const carSelections=document.querySelectorAll('.car-option');
+
+let arr=[];
+carSelections.forEach((v)=>{
+    arr.push(v.textContent);
+})
+
+for(let i=0;i<carSelections.length;i++){
+    carSelections[i].addEventListener('click',function (){
+
+        searchFieldValue.value=this.textContent;
+
+    })
+}
+typeSearch.addEventListener('keyup',function (){
+    const carSelections=document.getElementById("car-selection-div");
+    const selections=document.querySelectorAll('.car-option')
+    typeSearch.classList.remove('type-search-icon');
+    typeSearch.classList.add('type-search-reset-icon');
+    for(let sel of selections){
+        if(!sel.textContent.includes(typeSearch.value)){
+            sel.style.display='none';
+            if(typeSearch.value==="")
+                typeSearch.classList.remove('type-search-reset-icon');
+                typeSearch.classList.add('type-search-icon');
+                sel.style.display='block';
+                sel.style.backgroundColor="white";
+                sel.addEventListener('mouseover',()=>{
+                    sel.style.backgroundColor='#5FD36C';
+                })
+                sel.addEventListener('mouseout',()=>{
+                    sel.style.backgroundColor='white';
+                })
+            }
+        }
+        if(typeSearch.value===""){
+            for (let sel of selections){
+                sel.style.display='block';
+            }
+        }
+})
